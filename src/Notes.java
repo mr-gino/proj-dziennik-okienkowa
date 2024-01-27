8d365df5df1930680e77c9a6584d0ac5ea00f6d4
mr-gino <skocz.dawid@o2.pl>
2024-01-27 01:15:16 +0100

    Dodaje klase obiektowa Notes

---
+++ src/Notes.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notes {
+    private int userId;
+    private List<String> notes;
+
+    public Notes(int userId) {
+        this.userId = userId;
+        this.notes = new ArrayList<>();
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void addNoteToList(String newNote) {
+        notes.add(newNote);
+    }
+}
